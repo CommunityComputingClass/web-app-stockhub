3ec66cbc7eb3ff50f2e2ddbbbfd09913eb430762
KayinJules <127335243+KayinJules@users.noreply.github.com>
2025-03-17 18:17:30 +0000

    stockhubfirst

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,8 +1,14 @@
 function setup() {
-  createCanvas(400, 400);
-  background(200);
+
+  createCanvas(1080, 1920);
+
+  background(234,244,244);
 }
 
+///COLORS,   (1,22,39)-rich black (26,27,65)- Space Cadet (93,115,126)-payne's gray (234,244,244)-Azure(web) (204,227,222)- Mint Green
 function draw() {
-  // Nothing here yet – we'll add features step by step
+  // Nothing here yet – we'll add features step by step //108,54,864,216,1080,
+  fill(1,22,39)
+  rect (108,108, 864, 216)
+  rect (0,1728, 1080, 216)
 }
